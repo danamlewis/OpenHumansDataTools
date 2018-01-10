a6b0e4d49cf80f9bf1b14197a9fb7df90007fb79
Dana Lewis <danamlewis@users.noreply.github.com>
2018-01-10 18:19:09 -0500

    Update naming, and also adding headers to the output file

--- bin/devicestatus-pull-isf-timestamp.sh
+++ bin/devicestatus-pull-isf-timestamp.sh
@@ -12,25 +12,25 @@ set -eu
 
 ls -d [0-9]* | while read dir; do
 
-	echo "Creating files for participant" $dir 
-	
-	cd $dir/direct-sharing-31/${dir}_devicestatus_csv
-	
-	mkdir -p autotune_analysis
-	
-	ls -d *.csv | while read file; do
+    echo "Creating files for participant" $dir 
+   
+    cd $dir/direct-sharing-31/${dir}_devicestatus_*csv
+   
+    mkdir -p ISF_analysis
+    
+    ls -d *.csv | while read file; do
 
     #cut timestamp and isf values from the devicestatus files and save to a new csv file
-
-		csvcut -c openaps/enacted/timestamp $file > ${file%.csv}_timestamp.csv
-		csvcut -c openaps/enacted/reason $file | awk -F "\"*, \"*" '{print $4}' | sed 's/.*://' > ${file%.csv}_isf.csv
-		paste -d, ${file%.csv}_isf.csv <(cut -d, -f1- ${file%.csv}_timestamp.csv) > autotune_analysis/${file%.csv}_autotune.csv
-		rm ${file%.csv}_isf.csv ${file%.csv}_timestamp.csv
-
- 		echo ${file%.csv}_autotune.csv
-	 
-	done
-	
-	cd ../../../
-  
+        
+        echo "ISF,Timestamp" > ISF_analysis/${file%.csv}_isf.csv
+        csvcut -c openaps/enacted/timestamp $file > ${file%.csv}_timestamp.csv
+        csvcut -c openaps/enacted/reason $file | awk -F "\"*, \"*" '{print $4}' | sed 's/.*://' > ${file%.csv}_rawisf.csv
+        paste -d, ${file%.csv}_rawisf.csv <(cut -d, -f1- ${file%.csv}_timestamp.csv) >> ISF_analysis/${file%.csv}_isf.csv
+        rm ${file%.csv}_rawisf.csv ${file%.csv}_timestamp.csv
+        echo ${file%.csv}_isf.csv
+    
+    done
+   
+    cd ../../../
+ 
 done
